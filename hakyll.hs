6bf7a286ca830b4bd0fcdd58edc1cec8958b5916
eggcaker <eggcaker@gmail.com>
2013-05-17 00:44:51 +0800

    template changed

--- hakyll.hs
+++ hakyll.hs
@@ -1,24 +1,32 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
+import           Data.Monoid        (mappend, mconcat)
+import           Prelude            hiding (id)
+import           System.Cmd         (system)
+import           System.FilePath    (replaceExtension, takeDirectory)
+import qualified Text.Pandoc        as Pandoc
+-------------------------------------------------------------------------------
 import           Hakyll
 
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
-    match "assets/css/*" $ do
+main = hakyllWith config $ do
+    match "assets/**" $ do
         route   idRoute
         compile copyFileCompiler
-    match "assets/articles/*/*/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-    match "assets/img/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-    match "assets/js/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+
+    -- Build tags
+    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= saveSnapshot "content"
+            >>= loadAndApplyTemplate "templates/post.html"    (postCtx tags)
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
 
     match (fromList ["search.md", "reviews.md","puzzles.md"]) $ do
         route   $ setExtension "html"
@@ -30,54 +38,95 @@ main = hakyll $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+    -- render rss RSS feed
+    create ["rss.xml"] $ do
+        route idRoute
+        compile $ do
+           loadAllSnapshots "posts/*" "content"
+               >>= fmap (take 10) . recentFirst
+               >>= renderAtom (feedConfiguration "All posts") feedCtx
 
     create ["archive.html"] $ do
         route idRoute
         compile $ do
+            list <- postList tags "posts/*" recentFirst
             let archiveCtx =
-                    field "posts" (\_ -> postList recentFirst) `mappend`
-                    constField "title" "Archives"              `mappend`
+                    constField "title" "Archives" `mappend`
+                    constField "posts" list       `mappend`
                     defaultContext
 
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
+    
+   -- post tags
+    tagsRules tags $ \tag pattern -> do 
+        let title = "Posts tagged " ++ tag
 
+        -- copied from posts, need to refactor
+        route idRoute
+        compile $ do 
+          list <- postList tags pattern recentFirst
+          let ctx = constField "title" title `mappend`
+                    constField "posts" list `mappend`
+                    defaultContext
+          makeItem ""
+              >>= loadAndApplyTemplate "templates/archive.html" ctx
+              >>= loadAndApplyTemplate "templates/default.html" ctx
+              >>= relativizeUrls
 
     match "index.html" $ do
         route idRoute
         compile $ do
-            let indexCtx = field "posts" $ \_ ->
-                                postList $ fmap (take 10). recentFirst
+            list <- postList tags "posts/*" $ fmap (take 10) . recentFirst
+            let indexCtx = constField "posts" list `mappend`
+                 field "tags" (\_ -> renderTagList tags) `mappend`
+                 defaultContext
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" postCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
 
 
 --------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%Y-%m-%d" `mappend`
-    defaultContext
-
-
---------------------------------------------------------------------------------
-postList :: ([Item String] -> Compiler [Item String]) -> Compiler String
-postList sortFilter = do
-    posts   <- sortFilter =<< loadAll "posts/*"
-    itemTpl <- loadBody "templates/post-item.html"
-    list    <- applyTemplateList itemTpl postCtx posts
-    return list
+postCtx :: Tags -> Context String
+postCtx tags = mconcat
+    [ modificationTimeField "mtime" "%U"
+    , dateField "date" "%Y-%m-%d"
+    , tagsField "tags" tags
+    , defaultContext
+    ]
+-------------------------------------------------------------------------------
+feedCtx :: Context String
+feedCtx = mconcat
+  [
+    bodyField "description"
+    , defaultContext
+  ]
+-----------------------------------------------------------------------------
+feedConfiguration :: String -> FeedConfiguration
+feedConfiguration title = FeedConfiguration
+  {
+    feedTitle       = "iemacs - " ++ title
+  , feedDescription = "Personal blog of iemacs"
+  , feedAuthorName  = "eggcaker"
+  , feedAuthorEmail = "eggcaker@gmail.com"
+  , feedRoot        = "http://iemacs.com"
+  }
+-------------------------------------------------------------------------------
+config :: Configuration
+config = defaultConfiguration
+  {
+     deployCommand = "make publish"
+  }
+-------------------------------------------------------------------------------
+postList :: Tags -> Pattern ->([Item String] -> Compiler [Item String]) -> Compiler String
+postList tags pattern preprocess' = do
+    postItemTpl <- loadBody "templates/post-item.html"
+    posts       <- preprocess' =<< loadAll pattern
+    applyTemplateList postItemTpl (postCtx tags) posts
--- posts/2013-01-01-happy-new-year.md
+++ posts/2013-01-01-happy-new-year.md
@@ -1,5 +1,6 @@
 ---
 title: Happy new year 2013
+tags: Misc
 ---
 
 Happy new year! 2013 this time is !!!

4700bd4cdb88c75e8c41f171006703ad1c91f53c
张同柱 <zhangtongzhu@xiaoice.com>
2021-11-08 22:53:25 +0800

    add ditaa jar file

--- build-site.el
+++ build-site.el
@@ -4,20 +4,6 @@
 ;;
 ;; Author: eggcaker <https://github.com/eggcaker>
 ;; Maintainer: eggcaker <eggcaker@gmail.com>
-;; Created: November 07, 2021
-;; Modified: November 07, 2021
-;; Version: 0.0.1
-;; Keywords: blog emacs
-;; Homepage: https://github.com/eggcaker/
-;; Package-Requires: ((emacs "27.2"))
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; Commentary:
-;;
-;;  Description
-;;
-;;; Code:
 
 (require 'package)
 (setq package-user-dir (expand-file-name "./.packages"))
@@ -32,6 +18,20 @@
 ;; Install dependencies
 (package-install 'org)
 (package-install 'htmlize)
+
+(org-babel-do-load-languages
+ (quote org-babel-load-languages)
+ (quote ((emacs-lisp . t)
+         (ditaa . t)
+         (python . t)
+         (shell . t)
+         (js . t)
+         (org . t)
+         (plantuml . t)
+         )))
+
+
+
 (require 'org)
 (require 'ox-publish)
 
@@ -47,91 +47,64 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />"
+      emacs-cc-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" /><link rel=\"stylesheet\" href=\"/style.css\" type=\"text/css\"/>"
+      emacs-cc-html-preamble " <span><h1 class=\"brand\"><a href=\"/\">大道至簡</a></h1><span class=\"slogen\">咦！錢塘江上潮信來，今日方知我是我。</span></span> <nav> <ul class=\"menu\"> <li><a href=\"/\">Home</a></li> <li><a href=\"/pages/books\">Books</a></li> <li><a href=\"/pages/flags\">Flags</a></li> <li><a href=\"/pages/about\">About</a></li> </ul> <hr/> </nav>"
+      emacs-cc-html-postamble "<footer> <div class=\"generated\"> &copy; 2021-2028 TZ, built with %c | <a href=\"http://beian.miit.gov.cn/\">冀ICP备2020025756号-1</a> </div> </footer>"
 
-      )
+      org-ditaa-jar-path "./assets/ditaa.jar"
+      org-export-babel-evaluate t
+      org-confirm-babel-evaluate nil
+      org-html-doctype "html5")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
-      (list
-        (list  "posts"
-             :recursive t
-             :base-directory "./posts"
-             :base-extension "org"
-             :publishing-function 'org-html-publish-to-html
-             :publishing-directory "./_site"
-             :html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" /><link rel=\"stylesheet\" href=\"/style.css\" type=\"text/css\"/>"
-             :with-author nil           ;; Don't include author name
-             :with-creator t            ;; Include Emacs and Org versions in footer
-             :with-toc nil                ;; Include a table of contents
-             :section-numbers nil       ;; Don't include section numbers
-             :html-preamble "
-  <span><h1 class=\"brand\"><a href=\"/\">大道至簡</a></h1><span class=\"slogen\">咦！錢塘江上潮信來，今日方知我是我。</span></span>
-    <nav>
-    <ul class=\"menu\">
-      <li><a href=\"/\">Home</a></li>
-      <li><a href=\"/pages/books\">Books</a></li>
-      <li><a href=\"/pages/flags\">Flags</a></li>
-      <li><a href=\"/pages/about\">About</a></li>
-    </ul>
-    <hr/>
-    </nav>"
-             :html-postamble "
-<footer>
-  <div class=\"generated\">
-     &copy; 2021-2028 TZ, built with %c | <a href=\"http://beian.miit.gov.cn/\">冀ICP备2020025756号-1</a>
-  </div>
-</footer>"
-             :auto-index t
-             :index-title "[ Blogs]"
-             :index-filename "blogs.org"
-             :time-stamp-file t)
+      `(
+        ("posts"
+         :recursive t
+         :base-directory "./posts"
+         :base-extension "org"
+         :publishing-function org-html-publish-to-html
+         :publishing-directory "./_site"
+         :html-head ,emacs-cc-html-head
+         :with-author nil           ;; Don't include author name
+         :with-creator t            ;; Include Emacs and Org versions in footer
+         :with-toc nil                ;; Include a table of contents
+         :section-numbers nil       ;; Don't include section numbers
+         :html-preamble ,emacs-cc-html-preamble
+         :html-postamble ,emacs-cc-html-postamble
+         :auto-index t
+         :index-title "[ >>>Blogs]"
+         :index-filename "blogs.org"
+         :time-stamp-file t)
 
-      (list  "pages"
-             :recursive t
-             :base-directory "./pages"
-             :base-extension "org"
-             :publishing-function 'org-html-publish-to-html
-             :publishing-directory "./_site/pages"
-             :html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" /><link rel=\"stylesheet\" href=\"/style.css\" type=\"text/css\"/>"
-             :with-author nil           ;; Don't include author name
-             :with-creator t            ;; Include Emacs and Org versions in footer
-             :with-toc nil                ;; Include a table of contents
-             :with-title nil
-             :section-numbers nil       ;; Don't include section numbers
-             :html-preamble "
-  <span><h1 class=\"brand\"><a href=\"/\">大道至簡</a></h1><span class=\"slogen\">咦！錢塘江上潮信來，今日方知我是我。</span></span>
-    <nav>
-    <ul class=\"menu\">
-      <li><a href=\"/\">Home</a></li>
-      <li><a href=\"/pages/books\">Books</a></li>
-      <li><a href=\"/pages/flags\">Flags</a></li>
-      <li><a href=\"/pages/about\">About</a></li>
-    </ul>
-    <hr/>
-    </nav>"
-             :html-postamble "
-<footer>
-  <div class=\"generated\">
-     &copy; 2021-2028 Eggcaker, built with %c. | <a href=\"http://beian.miit.gov.cn/\">冀ICP备2020025756号-1</a>
-  </div>
-</footer>"
-             :time-stamp-file nil)
+        ("pages"
+         :recursive t
+         :base-directory "./pages"
+         :base-extension "org"
+         :publishing-function org-html-publish-to-html
+         :publishing-directory "./_site/pages"
+         :html-head ,emacs-cc-html-head
+         :with-author nil
+         :with-creator t
+         :with-toc nil
+         :with-title nil
+         :section-numbers nil
+         :html-preamble ,emacs-cc-html-preamble
+         :html-postamble ,emacs-cc-html-postamble
+         :time-stamp-file nil)
+        ("images"
+         :recursive t
+         :base-directory "./posts/images"
+         :base-extension "txt\\|jpg\\|gif\\|png"
+         :publishing-directory "./_site/images/"
+         :publishing-function org-publish-attachment)
 
-      (list "images"
-             :recursive t
-             :base-directory "./posts/images"
-             :base-extension (regexp-opt '("txt" "jpg" "gif" "png"))
-             :publishing-directory "./_site/images/"
-             :publishing-function 'org-publish-attachment)
-
-
-        (list "static"
-             :recursive nil
-             :base-directory "./assets"
-             :include '("CNAME" "favicon.ico" "style.css")
-             :publishing-directory "./_site"
-             :publishing-function 'org-publish-attachment)
+        ("static"
+         :recursive nil
+         :base-directory "./assets"
+         :include ,'("CNAME" "favicon.ico" "style.css")
+         :publishing-directory "./_site"
+         :publishing-function org-publish-attachment)
 
         ))
 

a4bf3136088817743ccb97c0c6122784071b9215
张同柱 <zhangtongzhu@xiaoice.com>
2021-11-10 14:52:44 +0800

    add comments system

--- build-site.el
+++ build-site.el
@@ -57,7 +57,7 @@
       emacs-cc-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" /><link rel=\"stylesheet\" href=\"/style.css\" type=\"text/css\"/>"
       emacs-cc-html-preamble " <span><h1 class=\"brand\"><a href=\"/\">大道至簡</a></h1><span class=\"slogen\">咦！錢塘江上潮信來，今日方知我是我。</span></span> <nav> <ul class=\"menu\"> <li><a href=\"/\">Home</a></li> <li><a href=\"/pages/books\">Books</a></li><li><a href=\"/pages/about\">About</a></li>  <li><a href=\"/rss.xml\">RSS</a></li> </ul> <hr/> </nav>"
       emacs-cc-html-postamble "<footer> <div class=\"generated\"> &copy; 2021-2028 TZ, built with %c | <a href=\"http://beian.miit.gov.cn/\">冀ICP备2020025756号-1</a> </div> </footer>"
-
+      emacs-cc-comments-html-postamble "<script src=\"https://utteranc.es/client.js\" repo=\"eggcaker/eggcaker.github.io\" issue-term=\"pathname\" theme=\"github-light\" crossorigin=\"anonymous\" async></script> <footer> <div class=\"generated\"> &copy; 2021-2028 TZ, built with %c | <a href=\"http://beian.miit.gov.cn/\">冀ICP备2020025756号-1</a> </div> </footer>"
       org-ditaa-jar-path (if (eq system-type 'windows-nt) "c:/temp/ditaa.jar" "/tmp/ditaa.jar")
       org-export-babel-evaluate t
       org-confirm-babel-evaluate nil
@@ -134,47 +134,79 @@ PROJECT is the current project."
          :recursive t
          :base-directory "./posts"
          :base-extension "org"
-         :exclude "rss\\.org\\|sitemap\\.org"
+         :exclude "rss\\.org\\|sitemap\\.org\\|index\\.org"
          :publishing-function org-html-publish-to-html
          :publishing-directory "./_site"
          :html-head ,emacs-cc-html-head
          :description "This is my personal website, a place where to me to write anything I want."
          :language en
-         :with-date t
+         :with-date nil
          :with-title t
          :headline-levels 4
-         :with-author nil           ;; Don't include author name
-         :with-creator t            ;; Include Emacs and Org versions in footer
-         :with-toc nil                ;; Include a table of contents
-         :section-numbers nil       ;; Don't include section numbers
+         :with-author nil
+         :with-creator nil
+         :with-toc nil
+         :section-numbers nil
          :html-preamble ,emacs-cc-html-preamble
-         :html-postamble ,emacs-cc-html-postamble
+         :html-postamble ,emacs-cc-comments-html-postamble
          :auto-sitemap t
          :sitemap-sort-files anti-chronologically
          :sitemap-format-entry cc/org-publish-org-sitemap-format-entry
          :htmlized-source t
          :html-doctype "html5"
          :html-html5-fancy t
-         :sitemap-title " >>>Blogs"
+         :sitemap-title nil
          :sitemap-filename "blogs.org"
+         :time-stamp-file t)
+        ("home"
+         :recursive t
+         :base-directory "./posts"
+         :base-extension "org"
+         :exclude "*.*"
+         :include ,'("index.org")
+         :publishing-function org-html-publish-to-html
+         :publishing-directory "./_site"
+         :html-head ,emacs-cc-html-head
+         :description "This is my personal website, a place where to me to write anything I want."
+         :language en
+         :with-date nil
+         :with-title t
+         :headline-levels 4
+         :with-author nil
+         :with-creator nil
+         :with-toc nil
+         :section-numbers nil
+         :html-preamble ,emacs-cc-html-preamble
+         :html-postamble ,emacs-cc-html-postamble
+         :auto-sitemap nil
+         :htmlized-source t
+         :html-doctype "html5"
+         :html-html5-fancy t
          :time-stamp-file t)
 
         ("rss"
          :recursive t
          :base-directory "./posts"
          :base-extension "org"
-         :exclude "rss\\.org\\|index\\.org\\|sitemap\\.org"
+         :exclude "rss\\.org\\|index\\.org\\|sitemap\\.org\\|blogs\\.org"
          :publishing-function cc/org-rss-publish-to-rss
          :publishing-directory "./_site"
          :html-head ,emacs-cc-html-head
-         :with-author nil           ;; Don't include author name
-         :with-creator t            ;; Include Emacs and Org versions in footer
-         :with-toc nil                ;; Include a table of contents
-         :section-numbers nil       ;; Don't include section numbers
+         :title "大道至简 - emacs.cc"
+         :description "This is my personal website, a place where to me to write anything I want."
+         :creator "eggcaker"
+         :with-author t
+         :with-creator t
+         :with-toc nil
+         :section-numbers nil
          :html-preamble ,emacs-cc-html-preamble
          :html-postamble ,emacs-cc-html-postamble
+         :sitemap-title "大道至简 - emacs.cc"
+         :sitemap-function cc/format-rss-feed
+         :sitemap-format-entry cc/format-rss-feed-entry
          :auto-sitemap t
          :sitemap-filename "rss.org"
+         :sitemap-style list
          :time-stamp-file t)
 
 

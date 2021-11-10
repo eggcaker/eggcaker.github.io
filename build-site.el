2cde449e181675fc27cb5bbb26d287934d3a0f50
张同柱 <zhangtongzhu@xiaoice.com>
2021-11-10 13:25:10 +0800

    add rss support

--- build-site.el
+++ build-site.el
@@ -7,8 +7,13 @@
 
 (require 'package)
 (setq package-user-dir (expand-file-name "./.packages"))
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(setq package-archives '(
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("nongnu-melpa" . "https://elpa.nongnu.org/nongnu/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")
+                         ))
+
+(add-to-list 'load-path (concat default-directory "./assets/lisp/"))
 
 ;; Initialize the package system
 (package-initialize)
@@ -18,6 +23,13 @@
 ;; Install dependencies
 (package-install 'org)
 (package-install 'htmlize)
+
+(require 'org)
+(require 'htmlize)
+(require 'ox-publish)
+(require 'ox-html)
+(require 'ox-rss)
+
 
 (org-babel-do-load-languages
  (quote org-babel-load-languages)
@@ -29,11 +41,6 @@
          (org . t)
          (plantuml . t)
          )))
-
-
-
-(require 'org)
-(require 'ox-publish)
 
 ;; UTF-8 as default encoding
 (prefer-coding-system 'utf-8)
@@ -48,13 +55,77 @@
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
       emacs-cc-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" /><link rel=\"stylesheet\" href=\"/style.css\" type=\"text/css\"/>"
-      emacs-cc-html-preamble " <span><h1 class=\"brand\"><a href=\"/\">大道至簡</a></h1><span class=\"slogen\">咦！錢塘江上潮信來，今日方知我是我。</span></span> <nav> <ul class=\"menu\"> <li><a href=\"/\">Home</a></li> <li><a href=\"/pages/books\">Books</a></li> <li><a href=\"/pages/flags\">Flags</a></li> <li><a href=\"/pages/about\">About</a></li> </ul> <hr/> </nav>"
+      emacs-cc-html-preamble " <span><h1 class=\"brand\"><a href=\"/\">大道至簡</a></h1><span class=\"slogen\">咦！錢塘江上潮信來，今日方知我是我。</span></span> <nav> <ul class=\"menu\"> <li><a href=\"/\">Home</a></li> <li><a href=\"/pages/books\">Books</a></li><li><a href=\"/pages/about\">About</a></li>  <li><a href=\"/rss.xml\">RSS</a></li> </ul> <hr/> </nav>"
       emacs-cc-html-postamble "<footer> <div class=\"generated\"> &copy; 2021-2028 TZ, built with %c | <a href=\"http://beian.miit.gov.cn/\">冀ICP备2020025756号-1</a> </div> </footer>"
 
-      org-ditaa-jar-path "/tmp/ditaa.jar"
+      org-ditaa-jar-path (if (eq system-type 'windows-nt) "c:/temp/ditaa.jar" "/tmp/ditaa.jar")
       org-export-babel-evaluate t
       org-confirm-babel-evaluate nil
       org-html-doctype "html5")
+
+(defun cc/org-publish-org-sitemap(title list)
+  "Sitemap generation function."
+  (concat "#+TITLE: Sitemap\n\n"
+          (org-list-to-subtree list)))
+
+(defun cc/org-publish-org-sitemap-format-entry (entry style project)
+  (cond ((not (directory-name-p entry))
+         (let* ((date (org-publish-find-date entry project)))
+           (format "%s - [[file:%s][%s]]"
+
+                   (format-time-string "%F" date) entry
+                   (org-publish-find-title entry project))))
+        ((eq style 'tree)
+         (file-name-nondirectory (directory-file-name entry)))
+        (t entry)))
+
+(defun cc/org-rss-publish-to-rss (plist filename pub-dir)
+  "Publish RSS with PLIST, only when FILENAME is 'rss.org'.
+PUB-DIR is when the output will be placed."
+  (if (equal "rss.org" (file-name-nondirectory filename))
+      (org-rss-publish-to-rss plist filename pub-dir)))
+
+(defun cc/format-rss-feed (title list)
+  "Generate RSS feed, as a string.
+TITLE is the title of the RSS feed.  LIST is an internal
+representation for the files to include, as returned by
+`org-list-to-lisp'.  PROJECT is the current project."
+  (concat "#+TITLE: " title "\n\n"
+          (org-list-to-subtree list 1 '(:icount "" :istart ""))))
+
+(defun cc/format-rss-feed-entry (entry style project)
+  "Format ENTRY for the RSS feed.
+ENTRY is a file name.  STYLE is either 'list' or 'tree'.
+PROJECT is the current project."
+  (cond ((not (directory-name-p entry))
+         (let* ((file (org-publish--expand-file-name entry project))
+                (title (org-publish-find-title entry project))
+                (date (format-time-string "%Y-%m-%d" (org-publish-find-date entry project)))
+                (link (concat (file-name-sans-extension entry) ".html")))
+           (with-temp-buffer
+             (insert (format "* %s\n" title))
+             (org-set-property "RSS_PERMALINK" link)
+             (org-set-property "PUBDATE" date)
+             (insert-file-contents file)
+             (buffer-string))))
+        ((eq style 'tree)
+         ;; Return only last subdir.
+         (file-name-nondirectory (directory-file-name entry)))
+        (t entry)))
+
+(defun templated-html-create-sitemap-xml (output directory base-url &rest regexp)
+  (let* ((rx (or regexp "\\.html")))
+    (with-temp-file output
+      (insert "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<urlset
+      xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"
+      xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+      xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">\n")
+      (cl-loop for file in (directory-files-recursively directory rx)
+            do (insert (format "<url>\n <loc>%s/%s</loc>\n <priority>0.5</priority>\n</url>\n"
+                               base-url (file-relative-name file directory))))
+      (insert "</urlset>"))))
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -63,7 +134,37 @@
          :recursive t
          :base-directory "./posts"
          :base-extension "org"
+         :exclude "rss\\.org\\|sitemap\\.org"
          :publishing-function org-html-publish-to-html
+         :publishing-directory "./_site"
+         :html-head ,emacs-cc-html-head
+         :description "This is my personal website, a place where to me to write anything I want."
+         :language en
+         :with-date t
+         :with-title t
+         :headline-levels 4
+         :with-author nil           ;; Don't include author name
+         :with-creator t            ;; Include Emacs and Org versions in footer
+         :with-toc nil                ;; Include a table of contents
+         :section-numbers nil       ;; Don't include section numbers
+         :html-preamble ,emacs-cc-html-preamble
+         :html-postamble ,emacs-cc-html-postamble
+         :auto-sitemap t
+         :sitemap-sort-files anti-chronologically
+         :sitemap-format-entry cc/org-publish-org-sitemap-format-entry
+         :htmlized-source t
+         :html-doctype "html5"
+         :html-html5-fancy t
+         :sitemap-title " >>>Blogs"
+         :sitemap-filename "blogs.org"
+         :time-stamp-file t)
+
+        ("rss"
+         :recursive t
+         :base-directory "./posts"
+         :base-extension "org"
+         :exclude "rss\\.org\\|index\\.org\\|sitemap\\.org"
+         :publishing-function cc/org-rss-publish-to-rss
          :publishing-directory "./_site"
          :html-head ,emacs-cc-html-head
          :with-author nil           ;; Don't include author name
@@ -73,9 +174,9 @@
          :html-preamble ,emacs-cc-html-preamble
          :html-postamble ,emacs-cc-html-postamble
          :auto-sitemap t
-         :sitemap-title " >>>Blogs"
-         :sitemap-filename "blogs.org"
+         :sitemap-filename "rss.org"
          :time-stamp-file t)
+
 
         ("pages"
          :recursive t
